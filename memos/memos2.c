c1110682a6d9b63a815d8d76e5c3c7ccac8aad27
Haodong <davidchd@outlook.com>
2023-10-17 04:11:39 -0400

    hello done;

--- memos/memos2.c
+++ memos/memos2.c
@@ -1,18 +1,9 @@
 char *video = (char *)0xB8000;
 
 void print_memory_size(unsigned int size) {
-    char *msg = "RAM Size (KB): ";
+    char *msg = "Hello There!";
     while (*msg) {
         *video++ = *msg++;
-        *video++ = 0x07;
-    }
-
-    char buffer[10];
-    itoa(size, buffer);
-
-    char *num = buffer;
-    while (*num) {
-        *video++ = *num++;
         *video++ = 0x07;
     }
 }
--- memos/memos2.s
+++ memos/memos2.s
@@ -11,27 +11,30 @@ _start:
 gdt_start:
 	.quad 0x0
 gdt_code:
-    .word 0xFFFF
-    .word 0x00
-    .byte 0x00
-    .byte 0b10011010
-    .byte 0b11001111
-    .byte 0x00
+	.word 0xFFFF
+	.word 0x00
+	.byte 0x00
+	.byte 0b10011010
+	.byte 0b11001111
+	.byte 0x00
 gdt_data:
-    .word 0xFFFF
-    .word 0x00
-    .byte 0x00
-    .byte 0b10010010
-    .byte 0b11001111
-    .byte 0x00
+	.word 0xFFFF
+	.word 0x00
+	.byte 0x00
+	.byte 0b10010010
+	.byte 0b11001111
+	.byte 0x00
 gdt_end:
 
 gdt_ptr:
-    .word gdt_end - gdt_start - 1
-    .long gdt_start
+	.word gdt_end - gdt_start
+	.long gdt_start
 
 CODE_SEG = gdt_code - gdt_start
 DATA_SEG = gdt_data - gdt_start
+
+stack_space:
+	.space 0x1000
 
 real_start:
 	cli
@@ -46,16 +49,12 @@ protect_start:
 	mov $DATA_SEG, %ax
 	mov %ax, %ds
 	mov %ax, %es
-    mov %ax, %fs
-    mov %ax, %gs
-    mov %ax, %ss
-    mov $stack_space, %esp
+	mov %ax, %fs
+	mov %ax, %gs
+	mov %ax, %ss
+	mov $stack_space, %esp
 
 	call print_memory_size
-	add $4, %esp
 
 	hlt
 	jmp .-1
-
-stack_space:
-    .space 0x1000
