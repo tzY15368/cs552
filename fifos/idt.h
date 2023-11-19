c2336208b226575d7000de90383db76bc6612f24
Haodong <davidchd@outlook.com>
2023-11-19 04:36:04 -0500

    我他妈日你二舅姥爷为什么会多出来这么多interrupt

--- fifos/idt.h
+++ fifos/idt.h
@@ -8,45 +8,61 @@
 #define IDTSIZE 0xFF
 
 typedef struct {
-    unsigned short offset_low;
-    unsigned short selector;
-    unsigned char zero;
-    unsigned char flags;
-    unsigned short offset_high;
+	unsigned short offset_low;
+	unsigned short selector;
+	unsigned char zero;
+	unsigned char flags;
+	unsigned short offset_high;
 } __attribute__((packed)) idt_entry_t;
 
 typedef struct {
-    unsigned short limit;
-    unsigned long long base;
+	unsigned short limit;
+	unsigned long long base;
 } __attribute__((packed)) idtr_t;
 
 static idt_entry_t idt[256];
 static idtr_t idtr;
 
-void idt_set_gate(
-        unsigned short interrupt_number, 
-        unsigned long long offset, 
-        unsigned short selector, 
-        unsigned short type) {
-    idt[interrupt_number].offset_low = offset & 0xFFFF;
-    idt[interrupt_number].selector = selector;
-    idt[interrupt_number].zero = 0;
-    idt[interrupt_number].flags = type;
-    idt[interrupt_number].offset_high = offset >> 16;
+static inline unsigned char inb( unsigned short usPort ) {
+    unsigned char uch;
+    __asm__ volatile ( "inb %1,%0" : "=a" (uch) : "Nd" (usPort) );
+    return uch;
 }
 
-__attribute__ ((interrupt)) void exception_handler(unsigned long long interrupt_number, unsigned long long error_code) {
-    terminal_writestring("Exception!\n");
-    __asm__ volatile ("iret");
+static inline void outb( unsigned char uch, unsigned short usPort ) {
+    __asm__ volatile ( "outb %0,%1" : : "a" (uch), "Nd" (usPort) );
+}
+
+void idt_set_gate(
+		unsigned short interrupt_number, 
+		unsigned long long offset, 
+		unsigned short selector, 
+		unsigned short type) {
+	idt[interrupt_number].offset_low = offset & 0xFFFF;
+	idt[interrupt_number].selector = selector;
+	idt[interrupt_number].zero = 0;
+	idt[interrupt_number].flags = type;
+	idt[interrupt_number].offset_high = offset >> 16;
+}
+
+void interrupt_handler(unsigned long long interrupt_number) {
+	terminal_writestring("Interrupt: ");
+	terminal_writeint(interrupt_number);
+	terminal_writestring("\n");
+	// if(interrupt_number < 8) 
+	// 	outb(0x20, 0x20);
+	// else if(interrupt_number < 16)
+	// 	outb(0x20, 0xA0);
+	__asm__ volatile ("iret");
 }
 
 void idt_init() {
-    __asm__ volatile ("cli");
-    idtr.limit = sizeof(idt) - 1;
-    idtr.base = (unsigned long long)idt;
-    for(int i = 0; i < 256; i++) {
-        idt_set_gate(i, (unsigned long long)exception_handler, 0x08, 0x8E);
-    }
-    __asm__ volatile ("lidt %0" : : "m" (idtr));
-    __asm__ volatile ("sti");
+	__asm__ volatile ("cli");
+	idtr.limit = sizeof(idt) - 1;
+	idtr.base = (unsigned long long)idt;
+	for(int i = 0; i < 256; i++) {
+		idt_set_gate(i, (unsigned long long)interrupt_handler, 0x08, 0x8E);
+	}
+	__asm__ volatile ("lidt %0" : : "m" (idtr));
+	__asm__ volatile ("sti");
 }
