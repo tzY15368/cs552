827856fa3621d14cdb0b0a6ce15c7d58f167762e
tzy <tzy15368@outlook.com>
2023-12-14 07:41:54 -0500

    lint fs

--- fifos/fs.h
+++ fifos/fs.h
@@ -107,18 +107,18 @@ int rd_unlink(char *pathname){
     int inode_no = dir_walk(pathname, FALSE, -1);
     if(inode_no <= 0) {
         
-    __asm__ __volatile__("sti");
+        __asm__ __volatile__("sti");
         return -1;
     };
     inode_t* inode = &ramfs->inode[inode_no];
     if(inode->type == INODE_TYPE_DIR && inode->size != 0){
         tprintf("dir not empty");
-    __asm__ __volatile__("sti");
+        __asm__ __volatile__("sti");
         return -1;
     }
     if(inode->ref_cnt != 0){
         tprintf("inode in use");
-    __asm__ __volatile__("sti");
+        __asm__ __volatile__("sti");
         return -1;
     }
     
@@ -186,11 +186,11 @@ int rd_close(int fd){
     __asm__ __volatile__("sti");
     return 0;
 }
+
 int rd_read(int fd, char *address, int num_bytes){
     __asm__ __volatile__("cli");
     if(fd < 0){
-        
-    __asm__ __volatile__("sti");
+        __asm__ __volatile__("sti");
         return -1;
     }
     thread_ctl_blk_t* cur_tcb = get_current_tcb(TRUE);
@@ -198,7 +198,7 @@ int rd_read(int fd, char *address, int num_bytes){
     if(file_descriptor->in_use == FALSE) return -1;
     int r = inode_read_bytes(file_descriptor->inode, file_descriptor->offset, address, num_bytes);
     if(r == -1){
-    __asm__ __volatile__("sti");
+        __asm__ __volatile__("sti");
         return -1;
     }
     file_descriptor->offset = min(file_descriptor->offset + num_bytes, file_descriptor->inode->size);
@@ -206,12 +206,12 @@ int rd_read(int fd, char *address, int num_bytes){
     __asm__ __volatile__("sti");
     return r;
 }
+
 int rd_write(int fd, char *address, int num_bytes){
     __asm__ __volatile__("cli");
     if(fd < 0){
-        
-    __asm__ __volatile__("sti");
-     return -1;
+        __asm__ __volatile__("sti");
+        return -1;
     }
     tprintf("n bytes: %d\n", num_bytes);
     thread_ctl_blk_t* cur_tcb = get_current_tcb(TRUE);
@@ -219,7 +219,7 @@ int rd_write(int fd, char *address, int num_bytes){
     if(file_descriptor->in_use == FALSE) return -1;
     int r = inode_write_bytes(file_descriptor->inode, file_descriptor->offset, address, num_bytes);
     if(r == -1){
-    __asm__ __volatile__("sti");
+        __asm__ __volatile__("sti");
         return -1;
     }
     file_descriptor->offset += num_bytes;
@@ -228,18 +228,18 @@ int rd_write(int fd, char *address, int num_bytes){
 int rd_lseek(int fd, int offset){
     __asm__ __volatile__("cli");
     if(fd < 0){
-        
-    __asm__ __volatile__("sti");
+        __asm__ __volatile__("sti");
         return -1;
     }
     thread_ctl_blk_t* cur_tcb = get_current_tcb(TRUE);
     file_descriptor_t* file_descriptor = &cur_tcb->fds[fd];
     if(file_descriptor->in_use == FALSE) {
-        
-    __asm__ __volatile__("sti");return -1;
+        __asm__ __volatile__("sti");
+        return -1;
     }
     if(offset < 0 || offset > file_descriptor->inode->size) {
-    __asm__ __volatile__("sti");return -1;
+        __asm__ __volatile__("sti");
+        return -1;
     }
     file_descriptor->offset = offset;
     __asm__ __volatile__("sti");
@@ -248,26 +248,26 @@ int rd_lseek(int fd, int offset){
 int rd_readdir(int fd, char *address){
     __asm__ __volatile__("cli");
     if(fd < 0) {
-        
-        __asm__ __volatile__("sti");return -1;
+        __asm__ __volatile__("sti");
+        return -1;
     }
     thread_ctl_blk_t* cur_tcb = get_current_tcb(TRUE);
     file_descriptor_t* file_descriptor = &cur_tcb->fds[fd];
     if(file_descriptor->in_use == FALSE) {
         tprintf("rd_readdir: file_descriptor->in_use == FALSE\n");
         
-    __asm__ __volatile__("sti");
+        __asm__ __volatile__("sti");
         return -1;
     }
     // return 1 on ok, 0 on eof, -1 on error
     if(file_descriptor->offset >= file_descriptor->inode->size) {
         
-    __asm__ __volatile__("sti");
+        __asm__ __volatile__("sti");
         return 0;
     }
     int r = inode_read_bytes(file_descriptor->inode, file_descriptor->offset, address, sizeof(dir_entry_t));
     if(r == -1) {
-    __asm__ __volatile__("sti");
+        __asm__ __volatile__("sti");
         return -1;
     }
     file_descriptor->offset += sizeof(dir_entry_t);
